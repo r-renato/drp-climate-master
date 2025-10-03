24e2842e4f7f062db4f40de18ef83f2684dd683b
Renato Rossi <renatrossi@gmail.com>
2025-10-03 19:51:33 +0200

    Baseline

---
+++ custom_components/drp_climate_master_v2/helpers/plant.py
@@ -0,0 +1,13 @@
+#
+from __future__ import annotations
+
+from ..domain.models.plant import PlantSnapshot
+
+
+
+
+def take_plant_snapshot(entities_state: dict) -> PlantSnapshot:
+    """..."""
+
+
+
